02383f6c88e9e77d8efef5d548e668f34b053a7a
yapjinai <jinai.jyap@gmail.com>
2018-09-14 13:34:58 -0400

    woooooooooo ugly short

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,10 +1,11 @@
 class Text
 
-  attr_accessor :text, :unlengthened_words, :words_array, :current_word
+  attr_accessor :text, :unlengthened_words, :unshortened_words, :words_array, :current_word
 
   def initialize(text="")
     @text = text
     @unlengthened_words = []
+    @unshortened_words = []
     @words_array = split_into_words_array(text)
 
     @current_word
@@ -108,11 +109,11 @@ class Text
 
             # CAP - Y
             if cap?(word)
-              get_word_choice(wordlinks_list, true)
+              get_long_word_choice(wordlinks_list, true)
 
             # CAP - N
             else
-              get_word_choice(wordlinks_list, false)
+              get_long_word_choice(wordlinks_list, false)
             end # if cap?(word)
 
           # MULTIPLE POS - N
@@ -135,11 +136,11 @@ class Text
 
               # CAP - Y
               if cap?(word)
-                get_word_choice(matching_pos_list, true)
+                get_long_word_choice(matching_pos_list, true)
 
               # CAP - N
               else
-                get_word_choice(matching_pos_list, false)
+                get_long_word_choice(matching_pos_list, false)
 
               end # if cap?(word)
             end # if matching_pos_list.empty?
@@ -155,36 +156,37 @@ class Text
     new_text_object.text = new_words_array.join(" ")
     new_text_object
   end
-  def map_text_lengthen
+
+  def map_text_shorten_rand
     new_text_object = Text.new(self.text)
 
     new_words_array = self.words_array.map do |word|
       self.current_word = word
 
-      # SHORT - Y
-      if short_string?(word)
-        short_word_object = ShortWord.find_by(word: word.downcase)
-        wordlinks_list = WordLink.where(short_word: short_word_object)
+      # SHORT - N
+      if !short_string?(word)
+        long_word_object = LongWord.find_by(word: word.downcase)
+        wordlinks_list = WordLink.where(long_word: long_word_object)
 
         # SYNONYMS - N
         if wordlinks_list.empty?
-          unlengthened_words << word
+          unshortened_words << word
           word
 
         # SYNONYMS - Y
         else
-          tags_array = arrayify_string(short_word_object.tags)
+          tags_array = arrayify_string(long_word_object.tags)
 
           # MULTIPLE POS - Y
           if tags_array.length <= 1
 
             # CAP - Y
             if cap?(word)
-              get_word_choice(wordlinks_list, true)
+              cap(wordlinks_list.sample.short_word.word)
 
             # CAP - N
             else
-              get_word_choice(wordlinks_list, false)
+              wordlinks_list.sample.short_word.word
             end # if cap?(word)
 
           # MULTIPLE POS - N
@@ -193,13 +195,13 @@ class Text
             part_of_speech = get_text
             matching_pos_list = wordlinks_list.select do |word_link_object|
               # find words whose part of speech matches
-              tags_array = arrayify_string(word_link_object.long_word.tags)
+              tags_array = arrayify_string(word_link_object.short_word.tags)
               tags_array.include? part_of_speech
             end # end matching_pos_list = wordlinks_list.select do
 
             # SYNONYMS - N
             if matching_pos_list.empty?
-              unlengthened_words << word
+              unshortened_words << word
               word
 
             # SYNONYMS - Y
@@ -207,14 +209,11 @@ class Text
 
               # CAP - Y
               if cap?(word)
-                get_word_choice(matching_pos_list, true)
+                cap(matching_pos_list.sample.short_word.word)
 
               # CAP - N
               else
-
-
-
-                ce(matching_pos_list, false)
+                matching_pos_list.sample.short_word.word
 
               end # if cap?(word)
             end # if matching_pos_list.empty?
@@ -231,11 +230,82 @@ class Text
     new_text_object
   end
 
+  def map_text_shorten
+    new_text_object = Text.new(self.text)
+
+    new_words_array = self.words_array.map do |word|
+      self.current_word = word
+
+      # SHORT - N
+      if !short_string?(word)
+        long_word_object = LongWord.find_by(word: word.downcase)
+        wordlinks_list = WordLink.where(long_word: long_word_object)
+
+        # SYNONYMS - N
+        if wordlinks_list.empty?
+          unshortened_words << word
+          word
+
+        # SYNONYMS - Y
+        else
+          tags_array = arrayify_string(long_word_object.tags)
+
+          # MULTIPLE POS - Y
+          if tags_array.length <= 1
+            # CAP - Y
+            if cap?(word)
+              get_short_word_choice(wordlinks_list, true)
+
+            # CAP - N
+            else
+              get_short_word_choice(wordlinks_list, false)
+            end # if cap?(word)
+
+          # MULTIPLE POS - N
+          else
+            puts "Which of the following describes '#{word}': #{tags_array.join(", ")}?"
+            part_of_speech = get_text
+            matching_pos_list = wordlinks_list.select do |word_link_object|
+              # find words whose part of speech matches
+              tags_array = arrayify_string(word_link_object.short_word.tags)
+              tags_array.include? part_of_speech
+            end # end matching_pos_list = wordlinks_list.select do
+
+            # SYNONYMS - N
+            if matching_pos_list.empty?
+              unshortened_words << word
+              word
+
+            # SYNONYMS - Y
+            else
+
+              # CAP - Y
+              if cap?(word)
+                get_short_word_choice(matching_pos_list, true)
+
+              # CAP - N
+              else
+                get_short_word_choice(matching_pos_list, false)
+
+              end # if cap?(word)
+            end # if matching_pos_list.empty?
+          end # end if tags_array.length <= 1
+        end # end if wordlinks_list.empty?
+
+      # SHORT - Y
+      else
+        word
+      end # end if short_string?(word)
+    end # end new_words_array = words_array.map do |word|
+
+    new_text_object.text = new_words_array.join(" ")
+    new_text_object
+  end
 
 #############################
 
 
-  def get_word_choice(list_to_choose_from, cap=false)
+  def get_long_word_choice(list_to_choose_from, cap=false)
     puts "Type the number of the word you'd like to replace '#{self.current_word}':"
     puts "0) Do not replace"
     list_to_choose_from.each_with_index do |wl, i|
@@ -244,7 +314,7 @@ class Text
     input = get_text
     if !((/^[0-9]*$/.match(input)) && (0..list_to_choose_from.length).include?(input.to_i))
       puts "Invalid input!"
-      get_word_choice(list_to_choose_from, cap)
+      get_long_word_choice(list_to_choose_from, cap)
     else
       choice = input.to_i - 1
       if choice == -1
@@ -259,11 +329,43 @@ class Text
     end
   end
 
+  def get_short_word_choice(list_to_choose_from, cap=false)
+    puts "Type the number of the word you'd like to replace '#{self.current_word}':"
+    puts "0) Do not replace"
+    list_to_choose_from.each_with_index do |wl, i|
+      puts "#{i+1}) #{wl.short_word.word}"
+    end
+    input = get_text
+    if !((/^[0-9]*$/.match(input)) && (0..list_to_choose_from.length).include?(input.to_i))
+      puts "Invalid input!"
+      get_short_word_choice(list_to_choose_from, cap)
+    else
+      choice = input.to_i - 1
+      if choice == -1
+        self.current_word
+      else
+        if cap == true
+          cap(list_to_choose_from[choice].short_word.word)
+        elsif cap == false
+          list_to_choose_from[choice].short_word.word
+        end
+      end
+    end
+  end
+
   def print_unlengthened_words
     unlengthened_words_cleaned = self.unlengthened_words.select { |w| /[A-Za-z]+/.match(w) }
     if !unlengthened_words_cleaned.empty?
       puts "We were unable to lengthen the following words:"
       puts unlengthened_words_cleaned.join(", ")
+    end
+  end
+
+  def print_unshortened_words
+    unshortened_words_cleaned = self.unshortened_words.select { |w| /[A-Za-z]+/.match(w) }
+    if !unshortened_words_cleaned.empty?
+      puts "We were unable to lengthen the following words:"
+      puts unshortened_words_cleaned.join(", ")
     end
   end
 
--- lib/editor.rb
+++ lib/editor.rb
@@ -10,6 +10,18 @@ def rand_lengthen_essay(text_object)
   text_object.print_unlengthened_words
 end
 
+def choose_shorten_essay(text_object)
+  new_text_object = text_object.map_text_shorten
+  puts new_text_object.text
+  text_object.print_unshortened_words
+end
+
+def rand_shorten_essay(text_object)
+  new_text_object = text_object.map_text_shorten_rand
+  puts new_text_object.text
+  text_object.print_unshortened_words
+end
+
 def arrayify_string(string)
   string_without_brackets = string.sub("[\"", "").sub("\"]", "")
   array = string_without_brackets.split("\", \"")
