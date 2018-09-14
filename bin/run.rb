c470f601f2f61c5e0bd1f220099ff3455ffee656
yapjinai <jinai.jyap@gmail.com>
2018-09-14 14:18:05 -0400

    fixed spaces again

--- bin/run.rb
+++ bin/run.rb
@@ -15,3 +15,5 @@ require_relative "../app/models/word_link.rb"
 require_relative "../app/models/text.rb"
 
 run_essay_editor
+
+# A mote it is to trouble the mind's eye. In the most high and palmy state of Rome, A little ere the mightiest Julius fell, The graves stood tenantless and the sheeted dead Did squeak and gibber in the Roman streets As stars with trains of fire and dews of blood, Disasters in the sun, and the moist star Upon whose influence Neptune's empire stands Was sick almost to doomsday with eclipse. And even the like precurse of feared events, As harbingers preceding still the fates And prologue to the omen coming on, Have heaven and earth together demonstrated Unto our climatures and countrymen.
